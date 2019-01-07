60ec5fcdf5de11f2024d194b78bc70c22e028516
Hani Elabed <hani.elabed@gmail.com>
2019-01-07 14:14:50 -0600

    adds sendMessage() and wires it to the
    button in the UI.

--- app/src/main/java/com/rubywebworks/firstapp/MainActivity.kt
+++ app/src/main/java/com/rubywebworks/firstapp/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.rubywebworks.firstapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.rubywebworks.firstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        // do something in response to the button click
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
